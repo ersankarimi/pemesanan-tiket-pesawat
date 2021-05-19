c467740c591cc19d8c26bc7c46912d8d8871ca2f
sankarim <73420137+ersankarimi@users.noreply.github.com>
2021-05-19 16:11:42 +0800

    update (masih proses)

--- src/pemesanantiketpesawat/data/PilihKota.java
+++ src/pemesanantiketpesawat/data/PilihKota.java
@@ -1,34 +1,104 @@
 package pemesanantiketpesawat.data;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class PilihKota {
 
     // variabel
     private Scanner sc = new Scanner(System.in);
-    private List<String> daftarKota = new ArrayList<>();
+    private  List<String> daftarKota;
 
+    // variabel inputan
+    private byte pilKotaAsal;
+    private byte pilKotaTujuan;
+
+
+    // constructor
     public PilihKota(){
 
-        kotaAsal();
-    }
-
-    // method daftar-daftar kota
-    public void kotaDaftar(){
+        // add daftar kota
+        daftarKota = new ArrayList<>();
         daftarKota.addAll(List.of("1. Bandung", "2. Jakarta", "3. Medan", "4. Makassar", "5. Exit"));
-        for (var kota : daftarKota){
-            System.out.println(kota);
-        }
+
+        pilihKota();
     }
 
+
+    // method pilihan kota asal
     public void kotaAsal(){
         System.out.println("Silahkan Pilih Kota Asal Anda!");
         System.out.println("--------------------------------");
-        kotaDaftar();
+
+        // iterasi untuk daftarKota
+        for (var kota : daftarKota){
+            System.out.println(kota);
+        }
+
         System.out.println("--------------------------------");
         System.out.println(" ");
 
+        System.out.print("Masukkan Pilihan [1-5] : ");
+        this.pilKotaAsal = sc.nextByte();
+        System.out.println(" ");
+    }
+
+    // method pilihan kota tujuan
+    public void kotaTujuan(){
+        System.out.println("Silahkan Pilih Kota Tujuan Anda!");
+        System.out.println("--------------------------------");
+
+
+        //iterasi untuk daftarKota
+        for (var kota : daftarKota){
+            System.out.println(kota);
+        }
+
+        System.out.println("--------------------------------");
+        System.out.println(" ");
+
+        System.out.print("Masukkan Pilihan [1-5] : ");
+        this.pilKotaTujuan = sc.nextByte();
+        if (this.pilKotaTujuan == 5){
+            exitPil();
+        } else if (this.pilKotaTujuan > 5){
+            elsePil();
+        }
+        System.out.println(" ");
+
+    }
+
+    // method exit
+    private void exitPil() {
+        System.out.println("Terima Kasih Telah Menggunakan Program Kami");
+        System.exit(0);
+    }
+
+    // method jika pilihan tidak ada di daftar
+    private void elsePil() {
+        System.out.println("Maaf Pilihan Tidak Ada di Daftar!");
+        System.out.println("Silahkan Pilih Sesuai Pilihan yang Tersedia!");
+        System.out.println(" ");
+    }
+
+    // method
+    public void pilihKota(){
+//        var looping = true;
+        while (true){
+            kotaAsal();
+            if (this.pilKotaAsal == 1){
+                kotaTujuan();
+            } else if (this.pilKotaAsal == 2){
+                kotaTujuan();
+            } else if (this.pilKotaAsal == 3){
+                kotaTujuan();
+            } else if (this.pilKotaAsal == 4){
+                kotaTujuan();
+            } else if (this.pilKotaAsal == 5){
+                exitPil();
+            } else {
+                elsePil();
+            }
+
+        }
     }
 }
